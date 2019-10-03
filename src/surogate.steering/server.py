73a512b578577318e447cfefc2adb97115851d6d
J Henry <jhenry@healthline.com>
2019-10-03 14:16:57 -0400

    using flask.request to get command data

--- src/surogate.steering/server.py
+++ src/surogate.steering/server.py
@@ -5,12 +5,9 @@ app = Flask(__name__)
 
 @app.route('/steering', methods=["POST"])
 def steer():
-    request_data = request.json
-    print(request_data)
-    steering_command = jsonify(request_data)
-    print(steering_command)
+    steering_command = request.json
 
-    if(steering_command.direction == 'left'):
+    if(steering_command['direction'] == 'left'):
         steering.left()
     else:
         steering.right()
