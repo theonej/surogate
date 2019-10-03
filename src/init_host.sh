10abae8bc19f83ae7600d24856f5972fbb859a31
J Henry <jhenry@healthline.com>
2019-10-03 11:27:51 -0400

    adding server init file

---
+++ src/init_host.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get upgrade
+
+apt-get install python3-pip
+pip3 install --upgrade setuptools
+
+pip3 install RPI.GPIO
+
+pip3 install adafruit-blinka
+
+sudo pip3 install adafruit-circuitpython-pca9685
+sudo pip3 install adafruit-circuitpython-servokit
